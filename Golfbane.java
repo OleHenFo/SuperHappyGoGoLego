c17b2898a89d1593a217bc0512e1c6ecb2b16111
OleHenFo <olehenfo@stud.ntnu.no>
2016-09-08 13:33:02 +0200

    Add files via upload
    Fungerende sensortest

--- Golfbane.java
+++ Golfbane.java
@@ -1,10 +1,6 @@
-import lejos.hardware.Button;
 import lejos.hardware.motor.*;
 import lejos.hardware.lcd.*;
-import lejos.hardware.sensor.EV3TouchSensor;
-import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
-import lejos.hardware.sensor.EV3GyroSensor;
 import lejos.hardware.port.Port;
 import lejos.hardware.Brick;
 import lejos.hardware.BrickFinder;
@@ -15,24 +11,46 @@ import lejos.robotics.SampleProvider;
 import lejos.hardware.sensor.*;
 
 public class Golfbane{
-	public static void main(String[] args){
-		// Skjerm
+	public static void main(String[] args) throws Exception{
+		// Variabler
+		int stop = 0;
+		float dist = 0;
+
+		// Definer boks
+		Brick legogo = BrickFinder.getDefault();
 		EV3 ev3 = (EV3) BrickFinder.getLocal();
+
+		// Definer skjerm
 		TextLCD lcd = ev3.getTextLCD();
+
+		// Definer knapper
+		Keys keys = ev3.getKeys();
+
 		// Port
-		Port s4 = BrickFinder.getDefault().getPort("S4");
+		Port s4 = legogo.getPort("S4");
+
 		// Definer sensor
 		EV3UltrasonicSensor sensor = new EV3UltrasonicSensor(s4);
-		SampleProvider reader = sensor.getDistanceMode();
-		float[] data = new float[reader.sampleSize()];
-		// Set fart
-		Motor.B.setSpeed(450);
-		Motor.C.setSpeed(450);
+		SampleProvider leser = sensor.getDistanceMode();
+		float[] data = new float[leser.sampleSize()];
 
-		while (true){
-			Button.waitForAnyPress();
-			reader.fetchSample(data, 0);
-			System.out.println("Avstand: " + data[0]);
+		// Vent for knappetrykk for å starte
+		lcd.drawString("Trykk for starte", 0, 1);
+		keys.waitForAnyPress();
+
+		// Hoved loop
+		while (stop<=20){
+			stop+=stop;
+
+			// Hent data fra leser, legg i data plass 0
+			leser.fetchSample(data, 0);
+			dist = data[0];
+
+			// Skriv data fra plass 0
+			System.out.println("Avstand: " + dist);
+
+			// Sleep 1 sec
+			Thread.sleep(1000);
 		}
 	}
 }
