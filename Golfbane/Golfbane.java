54efe6279e820c2c1d3b7e2e21876497cbcccfcb
OleHenFo <olehenfo@stud.ntnu.no>
2016-09-13 12:14:21 +0200

    Ferdig golfbane, begynnelsen på tunnel

---
+++ Golfbane/Golfbane.java
@@ -0,0 +1,164 @@
+import lejos.hardware.motor.*;
+import lejos.hardware.lcd.*;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+//import lejos.hardware.sensor.NXTTouchSensor;
+import lejos.hardware.port.Port;
+import lejos.hardware.Brick;
+import lejos.hardware.BrickFinder;
+import lejos.hardware.ev3.EV3;
+import lejos.hardware.Keys;
+import lejos.hardware.Button;
+import lejos.hardware.sensor.SensorModes;
+import lejos.robotics.SampleProvider;
+import lejos.hardware.sensor.*;
+import lejos.hardware.Sound;
+import java.util.Random;
+
+public class Golfbane{
+	public static void main(String[] args) throws Exception{
+		/*// Thread for å stoppe program (kjører parallelt med resten)
+		new Thread("Stopper") {
+			@Override
+			public void run() {
+				while (true){
+					if (Button.ESCAPE.isDown() && Button.ENTER.isDown()){
+						System.exit(0);
+					}
+				}
+			}
+		}.start();*/
+
+		//variabler
+		boolean go = true;
+		float dist = 0;
+		int dir = 1;
+		Random rand = new Random();
+
+		// Definer boks
+		Brick legogo = BrickFinder.getDefault();
+		EV3 ev3 = (EV3) BrickFinder.getLocal();
+
+		// Definer skjerm
+		TextLCD lcd = ev3.getTextLCD();
+
+		// Definer knapper
+		Keys keys = ev3.getKeys();
+
+		// Port
+		Port s4 = legogo.getPort("S4");
+		//Port s1 = legogo.getPort("S1");
+		//Port s2 = legogo.getPort("S2");
+
+		// Definer ultra sensor
+		EV3UltrasonicSensor sensorU = new EV3UltrasonicSensor(s4);
+		SampleProvider leserU = sensorU.getDistanceMode();
+		float[] dataU = new float[leserU.sampleSize()];
+
+		/*// Definer trykksensor venstre
+		NXTTouchSensor tsv = new NXTTouchSensor(s1);
+		SampleProvider leserV = tsv.getTouchMode();
+		float[] dataV = new float[leserV.sampleSize()];
+
+		// Definer trykksensor venstre
+		NXTTouchSensor tsh = new NXTTouchSensor(s2);
+		SampleProvider leserH = tsh.getTouchMode();
+		float[] dataH = new float[leserH.sampleSize()];*/
+
+		// Vent på knapp enter for å starte
+		lcd.drawString("Trykk enter starte", 0, 1);
+		keys.waitForAnyPress();
+
+		// Hoved loop
+		lcd.drawString("Kjorer", 0,5);
+
+		Motor.B.setSpeed(400);
+		Motor.C.setSpeed(400);
+
+		Motor.B.forward();
+		Motor.C.forward();
+
+		go=true;
+		while (true){
+			/*// Trykksensor kode
+			leserV.fetchSample(dataV, 0);
+			if (dataV[0] == 1){
+				lcd.drawString("Venstre "+dataV[0], 0,5);
+				Motor.B.backward();
+				Motor.C.backward();
+				Thread.sleep(500);
+				Motor.B.forward();
+				Motor.C.backward();
+				Thread.sleep(500);
+				go = true;
+			}
+
+			leserH.fetchSample(dataH, 0);
+			if (dataH[0] == 1){
+				lcd.drawString("Høyre "+dataH[0], 0,5);
+				Motor.B.backward();
+				Motor.C.backward();
+				Thread.sleep(500);
+				Motor.B.backward();
+				Motor.C.forward();
+				Thread.sleep(500);
+				go = true;
+			}*/
+
+			// Hent data fra leser, legg i data plass 0
+			leserU.fetchSample(dataU, 0);
+			dist = dataU[0];
+
+			// Sjekk om sensor distanse er > 0.2
+			if (dist<0.2&!go){
+
+				// Skriv tekst
+				lcd.drawString("Svinger", 0,5);
+				//Motor.B.stop();
+				//Motor.C.stop();
+				//Sound.twoBeeps();
+
+				while(dist<0.4){
+
+					// Sving sakte
+					Motor.B.setSpeed(200);
+					Motor.C.setSpeed(200);
+
+					// Sving venstre eller høyre, ut fra random variabel
+					if (dir == 1){
+						// Høyre
+						Motor.B.forward();
+						Motor.C.backward();
+					} else {
+						// Venstre
+						Motor.B.backward();
+						Motor.C.forward();
+					}
+					Thread.sleep(300);
+
+					//Motor.B.stop();
+					//Motor.C.stop();
+
+					// Sjekk sensor
+					leserU.fetchSample(dataU, 0);
+					dist = dataU[0];
+				}
+
+				go=true;
+			} else if (dist>0.2&&go){
+
+				// Set random variabel
+				dir = rand.nextInt(2);
+
+				// Skriv tekst
+				lcd.drawString("Kjorer", 0,5);
+
+				Motor.B.setSpeed(450);
+				Motor.C.setSpeed(450);
+				// Kjør
+				Motor.B.forward();
+				Motor.C.forward();
+				go=false;
+			}
+		}
+	}
+}
---
+++ Tunnel/Tunnel.java
@@ -0,0 +1,79 @@
+import lejos.hardware.motor.*;
+import lejos.hardware.lcd.*;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.EV3GyroSensor;
+import lejos.hardware.sensor.NXTTouchSensor;
+import lejos.hardware.sensor.NXTLightSensor;
+import lejos.hardware.sensor.NXTColorSensor;
+import lejos.hardware.sensor.NXTSoundSensor;
+import lejos.hardware.sensor.NXTUltrasonicSensor;
+import lejos.hardware.port.Port;
+import lejos.hardware.Brick;
+import lejos.hardware.BrickFinder;
+import lejos.hardware.ev3.EV3;
+import lejos.hardware.Keys;
+import lejos.hardware.sensor.SensorModes;
+import lejos.robotics.SampleProvider;
+import lejos.hardware.sensor.*;
+import lejos.hardware.Sound;
+
+public class Tunnel{
+	public static void main(String[] args) throws Exception{
+		//variabler
+		boolean go = true;
+		float dist = 0;
+
+		// Definer boks
+		Brick legogo = BrickFinder.getDefault();
+		EV3 ev3 = (EV3) BrickFinder.getLocal();
+
+		// Definer skjerm
+		TextLCD lcd = ev3.getTextLCD();
+
+		// Definer knapper
+		Keys keys = ev3.getKeys();
+
+		// Definer porter
+		Port s1 = legogo.getPort("S1");
+		Port s2 = legogo.getPort("S2");
+		Port s3 = legogo.getPort("S3");
+		Port s4 = legogo.getPort("S4");
+
+		// Definer ultra sensor
+		EV3UltrasonicSensor sensorU = new EV3UltrasonicSensor(s4);
+		SampleProvider leserU = sensorU.getDistanceMode();
+		float[] dataU = new float[leserU.sampleSize()];
+
+		// Definer lyssensor
+		EV3ColorSensor sensorF = new EV3ColorSensor(s1);
+		SampleProvider leserF = sensorF.getMode("RGB");
+		float[] dataF = new float[leserF.sampleSize()];
+
+		// Definer lydsensor
+		NXTSoundSensor sensorL = new NXTSoundSensor(s3);
+		SampleProvider leserL = sensorL.getDBMode();
+		float[] dataL = new float[leserL.sampleSize()];
+
+		// Vent på knapp enter for å starte
+		lcd.drawString("Trykk enter starte", 0, 1);
+		keys.waitForAnyPress();
+
+		// Hoved loop
+		lcd.drawString("Kjorer", 0,5);
+
+		Motor.A.setSpeed(300);
+		Motor.B.setSpeed(300);
+		Motor.C.setSpeed(300);
+		Motor.D.setSpeed(300);
+
+		Motor.A.forward();
+		Motor.B.forward();
+		Motor.C.forward();
+		Motor.D.forward();
+
+		while (keys.readButtons()==0){
+		}
+	}
+}
